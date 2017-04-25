c1a4280d7adde739ba778ba945fc04ebf3e1d362
Bradley Davies <davb3_15@uni.worc.ac.uk>
2017-04-25 15:05:08 +0100

    AskQuestion form ready to rock n roll

--- src/components/AskQuestion.vue
+++ src/components/AskQuestion.vue
@@ -10,6 +10,20 @@
       <label>Question</label>
       <md-textarea v-model="content"></md-textarea>
     </md-input-container>
+    <div class="section">
+      <div class="flex">
+        <md-switch v-model="anonymous">Post Anonymously?</md-switch>
+        <p :class="{ anonymous : anonymous }">
+          <md-icon v-if="!anonymous">warning</md-icon>
+          <span v-if="anonymous">This question will be asked anonymously on your behalf.</span>
+          <span v-else>This question will not be asked anonymously, and your username will be visible to other users.</span>
+        </p>        
+      </div>
+      <div>
+        <md-button class="md-raised md-primary">
+          <md-icon>add_circle</md-icon> Post Question</md-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,7 +33,8 @@ export default {
   data () {
     return {
       title: '',
-      content: ''
+      content: '',
+      anonymous: false
     }
   }
 }
@@ -32,5 +47,44 @@ export default {
   border-radius: 5px;
   border: 1px solid rgba(0, 0, 0, .1);
   background-color: white;
+
+  > .section {
+    display: flex;
+    align-items: stretch;
+    justify-content: center;
+    max-width: 900px;
+    margin: 0 auto;
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: .5rem;
+      margin: .5rem;
+      background: rgba(0, 0, 0, .05);
+      border-radius: 5px;
+      border: 1px solid rgba(0, 0, 0, .1);
+
+      &.flex {
+        flex: 1;
+      }
+
+      > p {
+        font-size: .7rem;
+        color: rgba(0, 0, 0, .5);
+        font-weight: bold;
+        margin: .5rem;
+        padding: .5rem;
+        border: 1px solid rgba(0, 0, 0, .1);
+        background: rgba(255, 30, 0, .2);
+        border-radius: 5px;
+
+        &.anonymous {
+          background: rgba(0, 255, 30, .2);
+        }
+      }
+    }
+  }
 }
 </style>
